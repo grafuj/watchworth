ddff296c7d451251d82716cd4e641a142986a9c4
Graham Fujisawa <sfrrki@gmail.com>
2023-04-18 15:21:20 -0700

    update db schema and seed

---
+++ db/seeds/01_films.sql
@@ -0,0 +1,4 @@
+INSERT INTO films (name, release_date, imdb_url, genre)
+VALUES ('samson','spoon@hotmail.com', 'password'),
+('graham', 'graham@outlook.com', 'password'),
+('julian', 'juelzlum@gmail.com', 'password');
