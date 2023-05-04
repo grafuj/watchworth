14ab77474bf557e36930d07690d8faf3e586ee94
Graham Fujisawa <sfrrki@gmail.com>
2023-05-04 14:54:29 -0700

    add dashboard

--- reactjs-frontend/src/App.js
+++ reactjs-frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Routes, Route } from "react-router-dom"; //BrowserRouter
+import { Routes, Route, BrowserRouter } from "react-router-dom"; //BrowserRouter
 import './App.css';
 function App() {
   const [state, setState] = useState({
@@ -15,11 +15,11 @@ function App() {
       axios.get("/api/reviews"),
     ])
       .then((all) => {
-        const folders = all[0].data;
-        const departments = all[1].data;
-        const items = all[2].data;
+        const films = all[0].data;
+        const users = all[1].data;
+        const reviews = all[2].data;
 
-        setState((prev) => ({ ...prev, folders, departments, items }));
+        setState((prev) => ({ ...prev, films, users, reviews }));
       })
       .catch((error) => {
         console.error(error);
@@ -27,12 +27,27 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-      <h1>Watchworth</h1>
-      <a href="/films">Bring Me Films</a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <p>Nav placeholder</p>
+      <Routes>
+        <Route path="/" element={
+          <Dashboard
+            films={state.films}
+            users={state.users}
+            reviews={state.reviews}
+          />
+        }
+        >
+
+        </Route>
+      </Routes>
+      <div className="App">
+        <header className="App-header">
+          <h1>Watchworth</h1>
+          <a href="/films">Bring Me Films</a>
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
 
