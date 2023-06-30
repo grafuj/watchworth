f85a4a098e50a336a7483ed9cf40b7d0f6eb8ec0
Graham Fujisawa <sfrrki@gmail.com>
2023-06-30 15:18:53 -0700

    about description and category definition

--- reactjs-frontend/src/components/Nav.jsx
+++ reactjs-frontend/src/components/Nav.jsx
@@ -10,13 +10,14 @@ export default function Nav(props) {
         <Link to="/" className="site-logo-text">Watchworth</Link>
       </div>
       <span className="nav-content">
+        <Link to="/about" className="site-logo-text">About</Link>
         <Link to="/films" className="site-logo-text">Films</Link>
         <Link to="/reviews" className="site-logo-text">Reviews</Link>
         <Link to="/users" className="site-logo-text">Users</Link>
       </span>
       <span className="nav-options">
         <Link to="/settings" className="site-logo-text">Settings</Link>
-        <Link to="/user/1" className="site-logo-text">User</Link>
+        <Link to="/user/1" className="site-logo-text">User1</Link>
         <Link to="/logout" className="site-logo-text">Logout</Link>
       </span>
     </nav>
